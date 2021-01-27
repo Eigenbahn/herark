ae09f235af542fc2604dcfbe770611bab18af0b3
p3r7 <11557146+p3r7@users.noreply.github.com>
2021-01-27 14:59:27 +0100

    Fix bad version of timbre, upgrade to existing version of snmp4j

--- project.clj
+++ project.clj
@@ -6,16 +6,23 @@
   :repositories { "snmpj" "https://oosnmp.net/dist/release" }
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.stuartsierra/component "0.2.3"]
-                 [org.snmp4j/snmp4j "2.3.4"]
-                 [com.taoensso/timbre "4.0.2"]
-                 [slingshot "0.12.2"]
-                 [clj-time "0.10.0"]
-                 [environ "1.0.0"]
-                 [commons-daemon/commons-daemon "1.0.15"]
-                 [prismatic/schema "0.4.3"]
+                 [com.taoensso/encore "3.10.1"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/test.check "0.7.0"]
-                 [schema-gen "0.1.5"]]
+                 [slingshot "0.12.2"]
+                 [commons-daemon/commons-daemon "1.0.15"]
+
+                 [prismatic/schema "0.4.3"]
+                 [schema-gen "0.1.5"]
+
+                 [environ "1.0.0"]
+
+                 [clj-time "0.10.0"]
+
+                 [com.taoensso/timbre "4.10.0"
+                  :exclusions [com.taoensso/encore]]
+
+                 [org.snmp4j/snmp4j "2.5.0"]]
   :plugins [[lein-environ "1.0.0"]
             [codox "0.8.13"]]
   :codox {:output-dir "target/codox"
